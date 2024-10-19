13c95b57d421b168b7bc7386a4bf06ec4490755b
Engine codegen <codegen@enginelabs.ai>
2024-10-19 02:05:36 +0000

    Add HomeController with basic structure

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class HomeController
+{
+    public function index()
+    {
+        return "Welcome to the Slot Machine Game!";
+    }
+
+    public function play()
+    {
+        // TODO: Implement the slot machine game logic here
+        return "Spin the slot machine!";
+    }
+}
